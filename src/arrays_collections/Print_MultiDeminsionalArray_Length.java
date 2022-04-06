738c481b789152890875f58e3c6c99761a092480
MdQureshi01 <noorrqureshi@gmail.com>
2022-04-06 12:59:10 -0400

    additional bracnhes

--- src/arrays_collections/Print_MultiDeminsionalArray_Length.java
+++ src/arrays_collections/Print_MultiDeminsionalArray_Length.java
@@ -14,7 +14,7 @@ public class Print_MultiDeminsionalArray_Length {
 	    System.out.println(a[0].length);            //3
 	    System.out.println(a[1].length);            //4
 	    
-	    //develop added
+	    //develop added and added
 	}
  
 }
